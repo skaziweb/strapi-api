14d0a480c77d18e27a651ee8fe35ddea2d39d25e
Aleksey Kuzmin <aleksey@novakidschool.com>
2022-02-10 01:51:26 +0700

    upd config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,15 @@
 const parse = require('pg-connection-string').parse;
 const config = parse(process.env.DATABASE_URL);
+console.log('DATABASE_URL', config)
 module.exports = ({ env }) => ({
     connection: {
     client: 'postgres',
     connection: {
-        host: config.host,
-        port: config.port,
-        database: config.database,
-        user: config.user,
-        password: config.password,
+        host: config.host || 'ec2-52-31-217-108.eu-west-1.compute.amazonaws.com',
+        port: config.port || 5432,
+        database: config.database || 'dljn77fg5bagq',
+        user: config.user || 'auppfynkftefqh',
+        password: config.password || '166e6d3b33c51e5090022bc60fa0c4c1e12472f5451d3eba9872f49395cce51f',
         ssl: {
             rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
         },
